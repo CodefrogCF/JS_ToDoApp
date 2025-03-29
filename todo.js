80e4f9f795ae190b3a3174702471d01e6b7ac8db
HagenK42 <666.thurisaz.666@gmail.com>
2025-03-29 15:24:27 +0100

    added functionality to add and clear button, first ui-revision

--- todo.js
+++ todo.js
@@ -1,3 +1,23 @@
-function test() {
+var input = document.getElementById("task-input");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+    addTodo();
+    }
+});
+
+function frog() {
     console.log("Ribbit!");
+}
+
+function addTodo() {
+    let task = document.getElementById("task-input").value;
+    let todoList = document.querySelector("ul");
+    let li = document.createElement("li");
+    li.innerText = task;
+    todoList.appendChild(li);
+}
+
+function clearList() {
+    let todoList = document.querySelector("ul");
+    todoList.innerHTML = "";
 }
